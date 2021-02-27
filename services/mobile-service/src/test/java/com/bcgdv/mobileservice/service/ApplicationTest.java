866427e831f49135a37ab416c40e63a30b537024
Stas Gorobzeev <staszp@gmail.com>
2021-02-27 14:51:46 +0200

    init project

---
+++ services/mobile-service/src/test/java/com/bcgdv/mobileservice/service/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.bcgdv.mobileservice.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
